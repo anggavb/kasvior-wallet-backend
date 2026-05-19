dfc662f07c7f543bf7a492a44bbbc46519bf4862
anggavb <anggavb8@gmail.com>
2026-05-19 16:19:11 +0700

    feat(middleware): add CORS middleware for handling cross-origin requests

--- internal/router/main.router.go
+++ internal/router/main.router.go
@@ -6,12 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/kasvior-wallet-backend/internal/dto"
+	"github.com/kasvior-wallet-backend/internal/middleware"
 )
 
 func InitRouter(router *gin.Engine, db *pgxpool.Pool) {
 	// middleware global
-	// router.Use(middleware.Logger)
-	// router.Use(middleware.CORSMiddleware)
+	router.Use(middleware.CORSMiddleware)
+
 	AuthRouter(router, db)
 	UserRouter(router, db)
 	TransferRouter(router, db)
