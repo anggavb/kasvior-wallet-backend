2778a1320e970abbbba5b6dabf79baefa51a9c43
anggavb <anggavb8@gmail.com>
2026-05-22 22:26:00 +0700

    refactor(pgconn): remove logging of DB_URL in ConnectDB function

--- internal/config/pgconn.go
+++ internal/config/pgconn.go
@@ -2,13 +2,11 @@ package config
 
 import (
 	"context"
-	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func ConnectDB() (*pgxpool.Pool, error) {
-	log.Println(os.Getenv("DB_URL"))
 	return pgxpool.New(context.Background(), os.Getenv("DB_URL"))
 }
