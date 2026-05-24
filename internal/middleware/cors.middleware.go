c497f4a0f17d7f887e015ec641a22d1892afaab8
anggavb <anggavb8@gmail.com>
2026-05-24 22:35:06 +0700

    fix(middleware): update allowed origin for CORS to localhost testing

--- internal/middleware/cors.middleware.go
+++ internal/middleware/cors.middleware.go
@@ -9,7 +9,7 @@ import (
 )
 
 func CORSMiddleware(ctx *gin.Context) {
-	allowedOrigin := []string{"http://127.0.0.1:5500"} // localhost testing origin
+	allowedOrigin := []string{"http://127.0.0.1:5173"} // localhost testing origin
 	currentOrigin := ctx.GetHeader("Origin")
 	if slices.Contains(allowedOrigin, currentOrigin) {
 		ctx.Header("Access-Control-Allow-Origin", currentOrigin)
