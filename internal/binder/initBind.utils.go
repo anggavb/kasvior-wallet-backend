3e178c5f7bccda047c5eba3d42d318fcaa676238
anggavb <anggavb8@gmail.com>
2026-05-26 11:19:50 +0700

    feat(binder): initialize validation and add custom validation for numeric and image_check tags

--- internal/binder/initBind.utils.go
+++ internal/binder/initBind.utils.go
@@ -1,6 +1,8 @@
 package binder
 
 import (
+	"log"
+	"mime/multipart"
 	"reflect"
 	"strings"
 
@@ -8,8 +10,14 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-func InitValidator() {
+// Initialize Binder Package
+func init() {
+	initValidate()
+}
+
+func initValidate() {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		// Register Tag Name Func
 		v.RegisterTagNameFunc(func(fld reflect.StructField) string {
 			name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 			if name == "" {
@@ -20,8 +28,10 @@ func InitValidator() {
 			}
 			return name
 		})
+		log.Println("RegisterTagNameFunc - get json/form struct tag")
 
-		_ = v.RegisterValidation("numeric", func(fl validator.FieldLevel) bool {
+		// Register Custom Validation for numeric tag
+		v.RegisterValidation("numeric", func(fl validator.FieldLevel) bool {
 			if fl.Field().Kind() != reflect.String {
 				return true
 			}
@@ -38,5 +48,31 @@ func InitValidator() {
 			}
 			return true
 		})
+		log.Println("RegisterValidation - Add custom validation for 'numeric' tag")
+
+		// Register Custom Validator for image_check tag
+		v.RegisterValidation("image_check", func(fl validator.FieldLevel) bool {
+			file, ok := fl.Field().Interface().(*multipart.FileHeader)
+			if !ok {
+				return false
+			}
+
+			// Example: Max size 2MB
+			if file.Size > 2*1024*1024 {
+				return false
+			}
+
+			// Example: Allowed types
+			allowedTypes := map[string]bool{
+				"image/jpeg": true,
+				"image/png":  true,
+				"image/bmp":  true,
+				"image/heic": true,
+			}
+
+			return allowedTypes[file.Header.Get("Content-Type")]
+		})
+		log.Println("RegisterValidation - Add custom validation for 'image_check' tag")
 	}
+
 }
